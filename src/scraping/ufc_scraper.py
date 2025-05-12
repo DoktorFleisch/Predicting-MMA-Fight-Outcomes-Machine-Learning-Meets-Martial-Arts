650f38721143ee4f4cb7e8703d0ae4baed822c56
Daniel Warkus <daniel.warkus@hhu.de>
2025-05-13 00:20:09 +0200

    Bugfixing

--- src/scraping/ufc_scraper.py
+++ src/scraping/ufc_scraper.py
@@ -5,10 +5,8 @@ import time
 
 BASE_URL = "http://ufcstats.com/statistics/events/completed/?page={}" #use .format to get the page
 
-website = requests.get(BASE_URL)
-
-def get_event_links(pages=2):
-    event_links = []
+def get_event_links(pages=2, delay=1):
+    all_event_links = []
 
     for page in range(pages):
         URL = BASE_URL.format(page)
@@ -18,7 +16,8 @@ def get_event_links(pages=2):
         links = soup.select('a.b-link')
 
         event_links = [link['href'] for link in links if 'href' in link.attrs]
+        all_event_links.extend(event_links)
 
-        time.sleep(1)
+        time.sleep(delay)
 
     return event_links
