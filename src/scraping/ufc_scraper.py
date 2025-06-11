c11f0264ad8ace93d5cc7aacb2ec3e1b1fd6cddb
DoktorFleisch <44265452+DoktorFleisch@users.noreply.github.com>
2025-06-11 17:54:45 +0200

    get_fighting_links function finished and get_fighting_data function started

--- src/scraping/ufc_scraper.py
+++ src/scraping/ufc_scraper.py
@@ -22,5 +22,34 @@ def get_event_links(pages=2, delay=1):
 
     return all_event_links
 
+def get_fighting_links(event_link, delay=1):
+    fighting_links = []
 
+    request = requests.get(event_link)
+    soup = BeautifulSoup(request.content, 'html_parser')
+
+    links = soup.select('a.blink')
+
+    fighting_links = [link['href'] for link in links if 'href' in link.attrs]
+
+    return fighting_links
+
+def get_fighting_data(fight_link):
+    request = requests.get(fight_link)
+    soup = BeautifulSoup(request.content, 'html_parser')
+
+    rows = soup.select('tr.b-fight-details__table-row tr')
+
+    return rows
+
+if __name__ == '__main__':
+    event_links = get_event_links()
+
+    first_event_link = event_links[1]
+    first_event_fights = get_fighting_links(first_event_link)
+    first_fight = first_event_fights[0]
+
+    rows = get_fighting_data(first_fight)
+
+    print(rows)
 
